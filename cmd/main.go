ebcafa2ec01dad04595ecbefe9c352691964dfa3
HeniTay <79849423+abdulkarimov@users.noreply.github.com>
2023-03-17 15:10:27 +0600

    cors

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,17 @@ package main
 import (
     "github.com/gofiber/fiber/v2"
     "github.com/abdulkarimov/onboarding/database"
+    "github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
     database.ConnectDb()
     app := fiber.New()
+    app.Use(cors.New(cors.Config{
+        AllowOrigins: "*",
+        AllowHeaders:  "*",
+    }))
 
     setupRoutes(app)
-
     app.Listen(":3000")
 }
