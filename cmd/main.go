27d27546cffe795e07921712ee7281d2316bdf36
liflaks <85747237+liflaks@users.noreply.github.com>
2023-04-15 13:31:32 +0600

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -15,7 +15,8 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     "*",
 		AllowCredentials: true,
-		AllowHeaders:     "Origin, Content-Type, Accept, Accept-Language, Content-Length",
+		AllowHeaders:     "Origin, Content-Type, Accept",
+		AllowMethods:     "GET, POST, HEAD, PUT, DELETE, PATCH, OPTIONS",
 	}))
 
 	app.Listen(":3000")
